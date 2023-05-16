3038f317d8364e2660134b725101b473b15da5b4
Robert-Penrod <122355139+Robert-Penrod@users.noreply.github.com>
2023-05-16 11:20:51 -0400

    Overflow test 2

--- src/components/BG.js
+++ src/components/BG.js
@@ -32,6 +32,7 @@ const BG = () => {
                     width: "100%",
                     height: "100vh",
                     background: `${Colors.bg3}CC`,
+                    overflow: "hidden",
                     zIndex: "-0.5",
                 }}
             ></div>
