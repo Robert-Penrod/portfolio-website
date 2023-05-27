25e71ff4f0e8e734354bb7eb43066ae115424f1c
Robert-Penrod <122355139+Robert-Penrod@users.noreply.github.com>
2023-05-27 13:33:01 -0400

    Title Changed

--- src/components/ProjectVisual.js
+++ src/components/ProjectVisual.js
@@ -18,46 +18,47 @@ const ProjectVisual = ({ projectData, maxSize, isMaxHeight }) => {
                       }
             }
         >
-            <div
-                style={{
-                    width: "100%",
-                    height: "100%",
-                }}
-            >
-                {projectData.video ? (
-                    <>
-                        <LazyVideo
-                            loop={true}
-                            autoplay={true}
-                            muted={true}
-                            src={`/videos/${projectData.video}`}
-                            type='video/mp4'
-                            width='100%'
-                            height='100%'
-                            controls={false}
-                            preload={true}
-                            style={{
-                                top: 0,
-                                zIndex: 1,
-                                display: "flex",
-                                justifyContent: "center",
-                                alignItems: "center",
-                                width: "100%",
-                                height: "100%",
-                                overflow: "hidden",
-                            }}
-                        />
-                    </>
-                ) : projectData.image ? (
-                    <img
-                        style={{ maxWidth: "100%" }}
-                        src={`/images/${projectData.image}`}
-                        alt='project'
-                    />
-                ) : (
-                    <div></div>
-                )}
-            </div>
+            {projectData.video ? (
+                // <LazyVideo
+                //     loop={true}
+                //     autoplay={true}
+                //     muted={true}
+                //     src={`/videos/${projectData.video}`}
+                //     type='video/mp4'
+                //     width='auto'
+                //     height='100%'
+                //     controls={false}
+                //     preload={true}
+                //     style={{
+                //         top: 0,
+                //         zIndex: 1,
+                //         display: "flex",
+                //         justifyContent: "center",
+                //         alignItems: "center",
+                //         width: "100%",
+                //         height: "100%",
+                //         overflow: "hidden",
+                //     }}
+                // />
+                <video
+                    className='mx-auto'
+                    autoPlay
+                    playsInline
+                    muted
+                    loop
+                    preload='auto'
+                    src={`/videos/${projectData.video}`}
+                    style={{ width: "100%" }}
+                />
+            ) : projectData.image ? (
+                <img
+                    style={{ maxWidth: "100%" }}
+                    src={`/images/${projectData.image}`}
+                    alt='project'
+                />
+            ) : (
+                <div></div>
+            )}
         </div>
     );
 };
